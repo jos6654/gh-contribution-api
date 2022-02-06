3cceb22f1d4a0efa54ceb1af830593f43d661bd2
jos6654 <jos6654@rit.edu>
2022-02-06 15:15:51 -0500

    Finished first draft of project

--- src/api/api_utils.py
+++ src/api/api_utils.py
@@ -1,17 +1,18 @@
-from more_itertools import last
 import requests
-import dateutil.parser
 from .config import token
 
 header = {'Authorization': 'token %s' % token}
 
-
+# Collect relevant statistics from user's repositories
+# username: str
+# forked: bool
 def collect_stats(username, forked):
-
     stats = {}
     page = 1
     last_page = False
     repositories = []
+
+    # Loop through all pages, 100 repositories at a time
     while not last_page:
         res = requests.get(f"https://api.github.com/users/{username}/repos?per_page=100&page={page}", headers=header)
         if forked:
@@ -22,16 +23,28 @@ def collect_stats(username, forked):
         
         page += 1   
         if len(res.json()) < 100: last_page = True
-    
+
     stats["num_repos"], stats["languages"] = len(repositories), {}
     stats["num_stargazers"] = stats["num_forks"] = stats["avg_repo_size"] = 0
+
+    # Iterate through repositories to collect stats
     for repo in repositories:
         stats["num_stargazers"] += repo["stargazers_count"]
         stats["num_forks"] += repo["forks_count"]
         stats["avg_repo_size"] += repo["size"]
         stats["languages"][repo["language"]] = stats["languages"].setdefault(repo["language"], 0) + 1
 
-    stats["avg_repo_size"] = int(stats["avg_repo_size"]/stats["num_repos"])
-
+    # Sort languages and make file size human readable
+    stats["languages"] = dict(sorted(stats["languages"].items(), key=lambda language: language[1], reverse=True))
+    stats["avg_repo_size"] = readable_file_size(stats["avg_repo_size"]/stats["num_repos"])
+    
     return stats
-    # A list of languages with their counts, sorted by the most used to least used
+
+# Format file size to be human readable with units, based off this solution: https://stackoverflow.com/a/1094933
+# size: float
+def readable_file_size(size):
+    for unit in ["KB", "MB", "GB"]:
+        if abs(size) < 1024.0:
+            return f"{size:3.1f} {unit}"
+        size /= 1024.0
+    return f"{size:.1f} TB"
